3ed85e912bc77cc6582228554807c6a493ab6443
Nils Langner <nils.langner@phmlabs.com>
2024-03-16 18:56:30 +0100

    config file

--- src/Command/FetchHealthStatusCommand.php
+++ src/Command/FetchHealthStatusCommand.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Business\Alerting\EmailAlertChannel;
+use App\Business\Alerting\AlertChannelFactory;
 use App\Business\Retriever\FileRetriever;
 use App\Business\Storage\FileStorage;
-use App\Business\Storage\NullStore;
 use App\Health\HealthStatus;
 use GuzzleHttp\Client;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
 #[AsCommand(
     name: 'app:health:fetch',
@@ -24,10 +24,13 @@ class FetchHealthStatusCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $configFile = __DIR__ . '/../../config/health.config.yaml';
+
         $retriever = new FileRetriever(__DIR__ . '/../../config/endpoints.csv');
 
-        $storage = new FileStorage(__DIR__. '/../../_storage');
-        $alertChannel = new EmailAlertChannel('nils.langner@startwind.io');
+        $storage = new FileStorage(__DIR__ . '/../../_storage');
+
+        $alertingChannels = $this->getAlertingChannels($configFile);
 
         $client = new Client();
 
@@ -37,16 +40,34 @@ class FetchHealthStatusCommand extends Command
             $array = json_decode((string)$response->getBody(), true);
 
             $array['_internal'] = [
-              'fetched' => time()
+                'fetched' => time()
             ];
 
             if ($array['status'] !== HealthStatus::SUCCESS) {
-                $alertChannel->sendAlert($array);
+                foreach ($alertingChannels as $alertingChannel) {
+                    $alertingChannel->sendAlert($alertingChannels);
+                }
             }
 
             $storage->storeHealthCheckResult($endpoint, $array);
         }
 
         return Command::SUCCESS;
+    }
+
+    /**
+     * @return \App\Business\Alerting\AlertChannel[]
+     */
+    protected function getAlertingChannels(string $configFile): array
+    {
+        $config = Yaml::parse(file_get_contents($configFile));
+
+        $channels = [];
+
+        foreach ($config['alerting']['channels'] as $channel) {
+            $channels[] = AlertChannelFactory::getAlertingChannel($channel['type'], $channel['options']);
+        }
+
+        return $channels;
     }
 }
