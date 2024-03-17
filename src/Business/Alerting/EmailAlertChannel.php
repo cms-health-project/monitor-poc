9b1aee8caf56c35784c4f1b0d3f9567e21abda44
Zeshan Ahmed <premiumsrapidshares@gmail.com>
2024-03-17 15:08:59 +0100

    Dynamic subjects

--- src/Business/Alerting/EmailAlertChannel.php
+++ src/Business/Alerting/EmailAlertChannel.php
@@ -12,23 +12,25 @@ class EmailAlertChannel extends AbstractAlertChannel
 {
     const ALERT_CHANNEL_IDENTIFIER = 'email';
 
+    private string $from = '';
     private array $emailAddresses;
 
     private string $theme = 'default';
 
     private Mailer $mailer;
-
     private array $defaultOptions = [
+        'from' => 'me@zeshanahmed.com',
         'subject' => [
-            'failed' => "Ohh no!!"
+            'failed' => "There is a critical error on your {endpointName}",
+            'success' => "Your endpoint ({endpointName}) is up and running again"
         ]
     ];
-
+    private array $subjects = [];
     private Environment $twig;
 
     public function __construct(array $options, Environment $twig)
     {
-        $options = array_merge_recursive($this->defaultOptions, $options);
+        $options = $this->array_merge_recursive_distinct($this->defaultOptions, $options);
 
         $this->twig = $twig;
 
@@ -44,6 +46,16 @@ class EmailAlertChannel extends AbstractAlertChannel
             $this->theme = $options['theme'];
         }
 
+        if (array_key_exists('from', $options)) {
+            $this->from = $options['from'];
+        }
+
+        if (array_key_exists('subject', $options)) {
+            foreach ($options['subject'] as $subjectKey => $subject) {
+                $this->subjects[$subjectKey] = $subject;
+            }
+        }
+
         $this->emailAddresses = $options['emailAddresses'];
 
         $this->mailer = new Mailer(Transport::fromDsn($dsn));
@@ -51,19 +63,52 @@ class EmailAlertChannel extends AbstractAlertChannel
 
     public function sendAlert(string $endpoint, array $healthCheckResult): void
     {
-        if ($healthCheckResult['status'] === HealthStatus::SUCCESS) {
-            $content = $this->twig->render('alerts/emails/' . $this->theme . '/success.html.twig', [$healthCheckResult]);
-        } else {
-            $content = $this->twig->render('alerts/emails/' . $this->theme . '/failed.html.twig', [$healthCheckResult]);
+        $errors = array();
+
+        foreach ( $healthCheckResult['checks'] as $checkResults ) {
+            foreach ( $checkResults as $checkResult ) {
+                if ( $checkResult['status'] !== HealthStatus::SUCCESS && $checkResult['output'] ) {
+                    $errors[] = $checkResult['output'];
+                }
+            }
         }
 
-        $email = (new Email())
-            ->from('me@zeshanahmed.com')
-            ->to('premiumsrapidshares@gmail.com')
-            ->bcc($this->emailAddresses[0])
-            ->subject('Alert')
-            ->text('This is a test email sent via Symfony Mailer.');
+        if ($healthCheckResult['status'] === HealthStatus::SUCCESS) {
+            $content = $this->twig->render('alerts/emails/' . $this->theme . '/success.html.twig', ['healthcheck' => $healthCheckResult, 'endpoint' => $endpoint]);
+            $subject = $this->subjects['success'];
+        } else {
+            $content = $this->twig->render('alerts/emails/' . $this->theme . '/failed.html.twig', ['healthcheck' => $healthCheckResult, 'endpoint' => $endpoint, 'errors' => $errors]);
+            $subject = $this->subjects['failed'];
+        }
 
-        $this->mailer->send($email);
+        foreach ($this->emailAddresses as $emailAddress) {
+            $email = (new Email())
+                ->from($this->from)
+                ->to($emailAddress)
+                ->subject($subject)
+                ->html($content);
+
+            $this->mailer->send($email);
+        }
+
+    }
+
+    public function array_merge_recursive_distinct( array &$array1, array &$array2 )
+    {
+      $merged = $array1;
+
+      foreach ( $array2 as $key => &$value )
+      {
+        if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) )
+        {
+          $merged [$key] = $this->array_merge_recursive_distinct ( $merged [$key], $value );
+        }
+        else
+        {
+          $merged [$key] = $value;
+        }
+      }
+
+      return $merged;
     }
 }
