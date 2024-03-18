7c2f7c40e754f0f70fafa27fa17d506b531c749d
Nils Langner <nils.langner@phmlabs.com>
2024-03-18 09:26:44 +0100

    clear errors

--- src/Controller/HealthController.php
+++ src/Controller/HealthController.php
@@ -18,9 +18,9 @@ class HealthController extends AbstractController
         $retriever = new YamlRetriever(__DIR__ . '/../../config/endpoints.yaml');
         $storage = new FileStorage(__DIR__ . '/../../_storage');
 
-        $healthStatuses = [];
 
         foreach ($retriever->getEndpoints() as $key => $endpoint) {
+            $healthStatuses = [];
             $status = $storage->getHealthCheckResult($key);
 
             foreach ($status['checks'] as $checkName => $checkList) {
