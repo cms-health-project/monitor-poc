7ddfb3070365670bcfa702792af84cd37843be81
Stefan Bürk <stefan@buerk.tech>
2025-03-16 19:41:41 +0100

    [TASK] Allow POST method, payload data for POST and headers in endpoints config
    To make it possible to protect the health check endpoints,
    the monitor must be able to make POST requests, allow to
    define headers to send along and also eventually additional
    request payload data.
    
    To accomplish that, the csv endpoint configuration is replaced
    with a php array for the simplicity and related code is adopted
    to deal with the slightly enhanced endpoint configuration more
    directly which also allows us to drop an obselete class now.
    
    Let's keep that simple for now.

---
+++ config/endpoints.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'plain_json_file' => [
+        'method' => 'GET',
+        'url' => 'https://www.thewebhatesme.com/health.json',
+        'headers' => [
+            'Content-Type' => 'application/json',
+        ],
+    ],
+    'drupal_endpoint_1' => [
+        'method' => 'GET',
+        'url' => 'https://drupal.cloudfest.buerk.tech/health-check',
+        'headers' => [
+            'Content-Type' => 'application/json',
+        ],
+    ],
+    'typo3_endpoint_1' => [
+        'method' => 'POST',
+        'url' => 'https://typo3.cloudfest.buerk.tech/typo3/reaction/1be7c244-c9c2-4542-a424-5d78222c4d0b',
+        'headers' => [
+            'Content-Type' => 'application/json',
+            'x-api-key' => '301ad60f0a4483644b60aa4bf8c901f9dce3fc1e',
+        ],
+    ],
+];
